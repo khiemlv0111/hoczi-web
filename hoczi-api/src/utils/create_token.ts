7b5564faf94e1461c63494d732edf1dd49bb567d
Le Toan <letoan285@gmail.com>
2026-04-12 16:47:48 +0700

    update

--- hoczi-api/src/utils/create_token.ts
+++ hoczi-api/src/utils/create_token.ts
@@ -3,7 +3,7 @@ import { JwtPayload } from '../middlewares/authMiddleware';
 
 
 export const createAccessToken = (payload: JwtPayload) => {
-    const expiredTime = process.env.NODE_ENV == "production" ? "7d" : "30d"; // 15m for production, 7d for development
+    const expiredTime = process.env.NODE_ENV == "production" ? "30d" : "30d"; // 15m for production, 7d for development
     return jwt.sign(payload, process.env.ACCESS_SECRET!, { expiresIn: expiredTime }); // 15m 
 };
 

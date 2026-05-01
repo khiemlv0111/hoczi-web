706f79fdbd685845ad85f3aafdc6ef62297b07ca
Le Toan <letoan285@gmail.com>
2026-05-01 08:18:19 +0700

    get rid of password

--- hoczi-api/src/repositories/userRepository.ts
+++ hoczi-api/src/repositories/userRepository.ts
@@ -91,8 +91,9 @@ class UserRepository {
     }
 
     async save(user: User) {
-
-        return this.repo.save(user);
+        const saved = await this.repo.save(user);
+        const { password, ...rest } = saved;
+        return rest;
     }
 
     async createInitUser(user: any) {
